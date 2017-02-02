757fbf8d107d5d6da9ae85706adad7039b123936
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-02-02 22:15:20 +0100

    Updated to 0.5.4 Updated to kotlin 1.1 beta 38

---
+++ pull_master.sh
@@ -0,0 +1,8 @@
+pushd korag; git checkout master; git pull; popd
+pushd korau; git checkout master; git pull; popd
+pushd korge; git checkout master; git pull; popd
+pushd korim; git checkout master; git pull; popd
+pushd korio; git checkout master; git pull; popd
+pushd korui; git checkout master; git pull; popd
+pushd korte; git checkout master; git pull; popd
+
