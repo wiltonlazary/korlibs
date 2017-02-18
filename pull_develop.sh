294db56d33c9f7c0aa12f24899436011475d91d0
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-02-18 19:31:48 +0100

    develop utility script

---
+++ pull_develop.sh
@@ -0,0 +1,8 @@
+pushd korag; git checkout develop; git pull; popd
+pushd korau; git checkout develop; git pull; popd
+pushd korge; git checkout develop; git pull; popd
+pushd korim; git checkout develop; git pull; popd
+pushd korio; git checkout develop; git pull; popd
+pushd korui; git checkout develop; git pull; popd
+pushd korte; git checkout develop; git pull; popd
+
